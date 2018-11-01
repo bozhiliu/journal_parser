48a7741bab890467c7e219ad021b7dad59bc9787
bozhi-desktop <bozhiliu@email.arizona.edu>
2018-11-01 05:38:23 -0700

    html parser

--- ieee.py
+++ ieee.py
@@ -2,6 +2,6 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 r = requests.get('https://www.ieee.org/publications/periodicals.html')
-soup = BS(r.text)
+soup = BS(r.text, 'html.parser')
 
-print(soup.prettify())
+print(soup.prettify())   
