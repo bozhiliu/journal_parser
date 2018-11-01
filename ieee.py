dff34e31438120fcf0920a74b1925eed1779e0e6
bozhi-desktop <bozhiliu@email.arizona.edu>
2018-11-01 12:51:06 -0700

    get table div

--- ieee.py
+++ ieee.py
@@ -4,6 +4,5 @@ from bs4 import BeautifulSoup as BS
 r = requests.get('https://www.ieee.org/publications/periodicals.html')
 soup = BS(r.text, 'html.parser')
 
-print(soup)
-
 div = soup.find('div',attrs={'class':'rte text parbase'})
+print(div)
