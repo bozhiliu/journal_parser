da9815b24db2f7451c97bf50cdf9a4cc9a443d48
bozhi-desktop <bozhiliu@email.arizona.edu>
2018-11-02 01:28:56 -0700

    save desc & subjects

--- ieee.py
+++ ieee.py
@@ -1,13 +1,19 @@
 import requests
 from bs4 import BeautifulSoup as BS
+from bs4 import NavigableString
 import os
 import json
+import pickle
 
 
 def IEEEdecode(obj):
     out = IEEEJournal(obj['href'], obj['name'])
     if 'impact' in obj:
         out.score(obj['impact'],obj['eigen'],obj['influence'])
+    if 'info' in obj:
+        out.description(obj['info'])
+    if 'subjectList' in obj:
+        out.subjects(obj['subjectList'])
     return out
 
 
@@ -22,6 +28,10 @@ class IEEEencoder(json.JSONEncoder):
             odict['impact'] = obj.impact
             odict['eigen'] = obj.eigen
             odict['influence'] = obj.influence
+        if hasattr(obj, 'info'):
+            odict['info'] = obj.info
+        if hasattr(obj, 'subjectList'):
+            odict['subjectList'] = obj.subjectList
         return odict
 
 
@@ -31,15 +41,28 @@ class IEEEJournal:
         self.name = name
 
     def __str__(self):
-        info = str(self.name.encode('utf-8'))
+        info = '\n' + str(self.name.encode('utf-8', 'xmlcharrefreplace'))
         if self.impact:
             info = info + ' | impact ' + str(self.impact) + ' | eigen ' + str(self.eigen) + ' | influence ' + str(self.influence)
+        if self.info:            
+            info = info + '\nInfo: ' + self.info.encode('utf-8')
+        if self.subjectList:
+            info = info + '\nSubjects: '
+            for ele in self.subjectList:
+                info = info + str(ele) + ' || '
+        info = info + '\n'
         return info
 
     def score(self, impact, eigen, influence):
         self.impact = impact
         self.eigen = eigen
         self.influence = influence
+
+    def description(self, desc):
+        self.info = desc
+
+    def subjects(self, subjectList):
+        self.subjectList = subjectList
 
 
 
@@ -67,26 +90,38 @@ else:
 #eigen: #journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-dkblu > span.num
 #influence: #journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-blu > span.num
 #scope: #journal-page-bdy > div.block.cf.jrn-aims-metrics > div.section.sec-style-a.jrnl-aims > div > span > a
-#description: #journal-page-bdy > div.block.cf.jrn-aims-metrics.no-feat > div.section.sec-style-a.jrnl-aims.compact > div > div > p:nth-child(2)
-#suject: #main > div > div:nth-child(9) > div.col-grd.col-1-grd > div > ul
+#description: #main > div > p:nth-child(4)
+#suject: #main > div > div:nth-child(13) > div.col-grd.col-1-grd > div > ul
 
 prefix = 'https://ieeexplore.ieee.org/xpl/'
+target = []
+if os.path.isfile(journal_pkl):
+    for j in journal_list:
+        r = requests.get(j.href)
+        soup = BS(r.text, 'html.parser')
+        impact = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-org > span.num')[0]
+        eigen = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-dkblu > span.num')[0]
+        influence = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-blu > span.num')[0]
+        j.score(impact.contents[0], eigen.contents[0], influence.contents[0])
+
+        scope = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.section.sec-style-a.jrnl-aims > div > span > a')[0]
+        r = requests.get(prefix+scope['href'])
+        soup = BS(r.text.encode('utf-8'), 'html.parser')
+        description = soup.select('#main > div.block.blk-style-wht.article-blk > p > p')[0]
+        subjects = [i.get_text() for i in soup.select('#main > div > div.col-2-290.cf.jrnl-abt-lists > div.col-grd.col-1-grd > div > ul > li')]
+        j.description(description.get_text())
+        j.subjects(subjects)
+
+    json.dump(journal_list, open(journal_pkl, 'w'), cls=IEEEencoder)
+else:
+    journal_list = json.load(open(journal_pkl, 'r'), object_hook=IEEEdecode)
+
+
 for j in journal_list:
-    r = requests.get(j.href)
-    soup = BS(r.text, 'html.parser')
-    impact = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-org > span.num')[0]
-    eigen = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-dkblu > span.num')[0]
-    influence = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.jrnl-metrics.cf > a.metric.bg-blu > span.num')[0]
-    j.score(impact.contents[0], eigen.contents[0], influence.contents[0])
-
-    scope = soup.select('#journal-page-bdy > div.block.cf.jrn-aims-metrics > div.section.sec-style-a.jrnl-aims > div > span > a')[0]
-    r = requests.get(prefix+scope['href'])
-
-
-
     print(j)
-    
     k = raw_input('Next...')
-    print(k.decode('ascii'))
+    if k == 1:
+        target.append(j.name)
+
 
 
